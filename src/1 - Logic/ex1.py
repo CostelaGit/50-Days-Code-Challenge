a2b7b09a015958756cb00582e60ef6efcc926aa9
Luiz Gustavo <luizgustavolb2021@gmail.com>
2025-05-19 23:36:57 -0300

    limpeza para o inicio do desafio

--- src/1 - Logic/ex1.py
+++
@@ -1,9 +0,0 @@
-def cpf_verify(cpf: str)->bool:
-    if len(cpf)!=11:
-        return False
-    else:
-        for i in cpf:
-            if i.is_integer():
-                return True   
-            else:
-                return False
